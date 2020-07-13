15b09bb44827c99f84fcdee134a38e36587d6e2d
HimbeersaftLP <mail@himbeer.me>
2020-07-13 16:21:43 +0200

    Fix checkbox logic, remove unneeded event handler

--- k2vr-installer-gui/Pages/Setup.xaml.cs
+++ k2vr-installer-gui/Pages/Setup.xaml.cs
@@ -135,7 +135,7 @@ namespace k2vr_installer_gui.Pages
 
             InstallerState settings = ((App)Application.Current).settings;
             settings.trackingDevice = device;
-            settings.allowAnalytics = checkBox_analytics.IsChecked == false;
+            settings.allowAnalytics = CheckBox_analytics.IsChecked == true;
             settings.installationPath = installLocation;
 
             settings.Write();
@@ -144,11 +144,6 @@ namespace k2vr_installer_gui.Pages
 
         public void OnSelected()
         {
-        }
-
-        private void ListBox_devices_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
     }
 }
