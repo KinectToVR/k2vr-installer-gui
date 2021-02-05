6bb647ac42dbc142efae1d0949613e0d8d8a0412
Hyblocker <bryan.spiteri@gmail.com>
2021-02-05 10:31:53 +0100

    visual studio fucking used tabs instead of spaces smh

--- k2vr-installer-gui/Tools/Utils.cs
+++ k2vr-installer-gui/Tools/Utils.cs
@@ -8,58 +8,58 @@ using System.Threading.Tasks;
 
 namespace k2vr_installer_gui.Tools
 {
-	static class Utils
-	{
-		public static bool IsDirectoryEmpty(string path)
-		{
-			return (Directory.GetFiles(path).Length + Directory.GetDirectories(path).Length) == 0;
-		}
+    static class Utils
+    {
+        public static bool IsDirectoryEmpty(string path)
+        {
+            return (Directory.GetFiles(path).Length + Directory.GetDirectories(path).Length) == 0;
+        }
 
-		#region Get Username
+        #region Get Username
 
-		// https://stackoverflow.com/questions/3197138#3641093
-		[DllImport("Kernel32.dll", SetLastError = true)]
-		[return: MarshalAs(UnmanagedType.U4)]
-		private static extern int WTSGetActiveConsoleSessionId();
+        // https://stackoverflow.com/questions/3197138#3641093
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.U4)]
+        private static extern int WTSGetActiveConsoleSessionId();
 
-		// https://smbadiwe.github.io/post/track-activities-windows-service/
-		[DllImport("Wtsapi32.dll")]
-		private static extern bool WTSQuerySessionInformation(IntPtr hServer, int sessionId, WtsInfoClass wtsInfoClass, out IntPtr ppBuffer, out int pBytesReturned);
-		[DllImport("Wtsapi32.dll")]
-		private static extern void WTSFreeMemory(IntPtr pointer);
+        // https://smbadiwe.github.io/post/track-activities-windows-service/
+        [DllImport("Wtsapi32.dll")]
+        private static extern bool WTSQuerySessionInformation(IntPtr hServer, int sessionId, WtsInfoClass wtsInfoClass, out IntPtr ppBuffer, out int pBytesReturned);
+        [DllImport("Wtsapi32.dll")]
+        private static extern void WTSFreeMemory(IntPtr pointer);
 
-		private enum WtsInfoClass
-		{
-			WTSUserName = 5,
-			WTSDomainName = 7,
-		}
+        private enum WtsInfoClass
+        {
+            WTSUserName = 5,
+            WTSDomainName = 7,
+        }
 
-		public static string GetUsername(int sessionId, bool prependDomain = true)
-		{
-			IntPtr buffer;
-			int strLen;
-			string username = "SYSTEM";
-			if (WTSQuerySessionInformation(IntPtr.Zero, sessionId, WtsInfoClass.WTSUserName, out buffer, out strLen) && strLen > 1)
-			{
-				username = Marshal.PtrToStringAnsi(buffer);
-				WTSFreeMemory(buffer);
-				if (prependDomain)
-				{
-					if (WTSQuerySessionInformation(IntPtr.Zero, sessionId, WtsInfoClass.WTSDomainName, out buffer, out strLen) && strLen > 1)
-					{
-						username = Marshal.PtrToStringAnsi(buffer) + "\\" + username;
-						WTSFreeMemory(buffer);
-					}
-				}
-			}
-			return username;
-		}
+        public static string GetUsername(int sessionId, bool prependDomain = true)
+        {
+            IntPtr buffer;
+            int strLen;
+            string username = "SYSTEM";
+            if (WTSQuerySessionInformation(IntPtr.Zero, sessionId, WtsInfoClass.WTSUserName, out buffer, out strLen) && strLen > 1)
+            {
+                username = Marshal.PtrToStringAnsi(buffer);
+                WTSFreeMemory(buffer);
+                if (prependDomain)
+                {
+                    if (WTSQuerySessionInformation(IntPtr.Zero, sessionId, WtsInfoClass.WTSDomainName, out buffer, out strLen) && strLen > 1)
+                    {
+                        username = Marshal.PtrToStringAnsi(buffer) + "\\" + username;
+                        WTSFreeMemory(buffer);
+                    }
+                }
+            }
+            return username;
+        }
 
-		public static string GetUsername()
-		{
-			return GetUsername(WTSGetActiveConsoleSessionId());
-		}
+        public static string GetUsername()
+        {
+            return GetUsername(WTSGetActiveConsoleSessionId());
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
